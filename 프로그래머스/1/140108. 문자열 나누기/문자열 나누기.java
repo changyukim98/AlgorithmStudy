e4596e1ef5ab7fde1ec2c9f1d9fcc157aa618981
Chan <33568800+changyukim98@users.noreply.github.com>
2023-11-29 09:06:05 +0900

    [level 1] Title: 문자열 나누기, Time: 0.65 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(String s) {
-        int answer = splitArray(s, 0, 0);
+        int answer = splitString(s, 0, 0);
         return answer;
     }
 
-    public int splitArray(String s, int i, int answer) {
+    public int splitString(String s, int i, int answer) {
         if (i >= s.length()) return answer;
 
-        int cnt1 = 1, cnt2 = 0;
+        int cnt1 = 1, cnt2 = 0, len = s.length();
         char x = s.charAt(i);
         i++;
 
-        for (; i < s.length(); i++) {
+        for (; i < len; i++) {
             if (s.charAt(i) == x) {
                 cnt1++;
             } else {
@@ -19,7 +19,7 @@ class Solution {
             }
 
             if (cnt1 == cnt2) {
-                return splitArray(s, i + 1, answer + 1);
+                return splitString(s, i + 1, answer + 1);
             }
         }
         return answer + 1;
