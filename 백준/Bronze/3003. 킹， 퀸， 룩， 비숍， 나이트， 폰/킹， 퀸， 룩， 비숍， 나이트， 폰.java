2a6e1417929910bccfba151f6135f6c5c4b8651c
Chan <33568800+changyukim98@users.noreply.github.com>
2023-12-04 11:43:16 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -3,19 +3,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[] chessPeace = {1, 1, 2, 2, 2, 8};
 
         String input = br.readLine();
         String[] myChessPeace = input.split(" ");
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 6; i++) {
             int myChessNum = Integer.parseInt(myChessPeace[i]);
-            bw.write(chessPeace[i] - myChessNum + " ");
+            sb.append(chessPeace[i] - myChessNum).append(" ");
         }
+        System.out.println(sb);
 
         br.close();
-        bw.flush();
-        bw.close();
     }
 }
