f5cf24e5ee3a00411275e7f365eae66cbd55de93
Chan <33568800+changyukim98@users.noreply.github.com>
2023-12-10 16:48:27 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static int N, M, max = 0;
@@ -12,20 +13,23 @@ public class Main {
         N = Integer.parseInt(info[0]);
         M = Integer.parseInt(info[1]);
 
-        String[] cards = br.readLine().split(" ");
+        int[] cards = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
         comb(cards, 0, 0, 0);
         System.out.println(max);
     }
 
-    public static void comb(String[] cards, int cnt, int cur, int sum) {
+    public static void comb(int[] cards, int cnt, int cur, int sum) {
         if (cnt == 3 && sum <= M) {
             max = Math.max(max, sum);
         } else {
             if (cur >= N) return;
             if (sum > M) return;
-            int curVal = Integer.parseInt(cards[cur]);
             comb(cards, cnt, cur + 1, sum);
-            comb(cards, cnt + 1, cur + 1, sum + curVal);
+            comb(cards, cnt + 1, cur + 1, sum + cards[cur]);
         }
     }
 }
