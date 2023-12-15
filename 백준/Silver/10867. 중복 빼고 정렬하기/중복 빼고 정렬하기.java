c2977350185dbb5967e3ac544693665135a3fde2
Chan <33568800+changyukim98@users.noreply.github.com>
2023-12-15 16:05:57 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 360 ms, Memory: 26852 KB -BaekjoonHub

--- 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 // https://www.acmicpc.net/problem/10867
 public class Main {
@@ -10,53 +11,17 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         String[] inputs = br.readLine().split(" ");
 
-        int[] arr = new int[N];
-        boolean[] isUsed = new boolean[2001];
-        int cnt = 0;
-
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(inputs[i]);
-            if (isUsed[num + 1000]) continue;
-            isUsed[num + 1000] = true;
-            arr[cnt++] = num;
+            set.add(Integer.parseInt(inputs[i]));
         }
-
-        quickSort(arr, 0, cnt - 1);
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < cnt; i++) {
-            sb.append(arr[i]).append(' ');
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append(' ');
         }
         System.out.println(sb);
-    }
-
-    public static void quickSort(int[] arr, int left, int right) {
-        if (left >= right) return;
-
-        int low = left + 1;
-        int high = right;
-
-        // left가 pivot
-        while (low <= high) {
-            if (arr[low] <= arr[left]) {
-                low++;
-                continue;
-            }
-            if (arr[high] >= arr[left]) {
-                high--;
-                continue;
-            }
-            swap(arr, low, high);
-        }
-
-        swap(arr, left, high);
-        quickSort(arr, left, high - 1);
-        quickSort(arr, high + 1, right);
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
