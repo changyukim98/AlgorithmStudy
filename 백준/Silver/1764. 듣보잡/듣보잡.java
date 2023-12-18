d77cef67ff2c76f1b584fff15ff41d1711c15769
Chan <33568800+changyukim98@users.noreply.github.com>
2023-12-18 17:39:04 +0900

    [Silver IV] Title: 듣보잡, Time: 392 ms, Memory: 28760 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,36 @@
+// https://www.acmicpc.net/problem/1764
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] info = br.readLine().split(" ");
+        int N = Integer.parseInt(info[0]);
+        int M = Integer.parseInt(info[1]);
+
+        Set<String> set = new HashSet<>();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String input = br.readLine();
+            if (set.contains(input)) list.add(input);
+        }
+        list.sort(String::compareTo);
+
+        StringBuilder sb = new StringBuilder(list.size() + "\n");
+        for (String s : list) {
+            sb.append(s).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
