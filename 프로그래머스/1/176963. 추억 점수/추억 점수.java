5146828981d02e7b4cf689901510c5ff8a00ea08
Chan <33568800+changyukim98@users.noreply.github.com>
2023-12-20 00:20:51 +0900

    [level 1] Title: 추억 점수, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+
+        int[] result = new int[photo.length];
+
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                sum += map.getOrDefault(photo[i][j], 0);
+            }
+            result[i] = sum;
+        }
+        
+        return result;
+    }
+}
