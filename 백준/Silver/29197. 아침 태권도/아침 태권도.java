9e39e1f696f1f4e67b20bbedd491aa0192141e81
Chan <33568800+changyukim98@users.noreply.github.com>
2023-12-23 08:09:07 +0900

    [Silver II] Title: 아침 태권도, Time: 100 ms, Memory: 12820 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Silver/29197. 아침 태권도/아침 태권도.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Map<Integer, Set<Integer>> setMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int gcd = calcGcd(x, y);
+
+            x /= gcd;
+            y /= gcd;
+            setMap.putIfAbsent(x, new HashSet<>());
+            Set<Integer> set = setMap.get(x);
+            set.add(y);
+        }
+
+        int cnt = 0;
+        for (Map.Entry<Integer, Set<Integer>> entry : setMap.entrySet()) {
+            cnt += entry.getValue().size();
+        }
+        System.out.println(cnt);
+    }
+
+    public static int calcGcd(int a, int b) {
+        if (a < b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
