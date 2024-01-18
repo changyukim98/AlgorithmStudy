acda445b59da21c73832e619d81e1c7591efe785
Chan <33568800+changyukim98@users.noreply.github.com>
2024-01-18 18:19:31 +0900

    [Gold V] Title: 트리 색칠하기, Time: 1480 ms, Memory: 112572 KB -BaekjoonHub

---
+++ 백준/Gold/24230. 트리 색칠하기/트리 색칠하기.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static List<Integer>[] edgesList;
+    private static boolean[] visited;
+    private static int[] colors;
+    private static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        edgesList = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            edgesList[i] = new ArrayList<>();
+        }
+        visited = new boolean[N + 1];
+        colors = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            colors[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int U = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            edgesList[U].add(V);
+            edgesList[V].add(U);
+        }
+        if (colors[1] != 0) answer++;
+        DFS(1, colors[1]);
+        System.out.println(answer);
+    }
+
+    public static void DFS(int cur, int color) {
+        visited[cur] = true;
+
+        for (Integer i : edgesList[cur]) {
+            if (visited[i]) continue;
+            if (colors[i] != color) answer++;
+            visited[i] = true;
+            DFS(i, colors[i]);
+        }
+    }
+}
