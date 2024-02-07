5e1afa2273698a1cc1b5278efef14347b9293c6c
Chan <33568800+changyukim98@users.noreply.github.com>
2024-02-07 09:26:24 +0900

    Delete 프로그래머스/unrated directory

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[end_num - start_num + 1];
-        for (int num = start_num, idx = 0; num <= end_num; num++, idx++) {
-            answer[idx] = num;
-        }
-        return answer;
-    }
-}
--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(int a, int b) {
-        String concat = String.valueOf(a) + String.valueOf(b);
-        int concatNum = Integer.parseInt(concat);
-
-        int answer = Math.max(2 * a * b, concatNum);
-        return answer;
-    }
-}
