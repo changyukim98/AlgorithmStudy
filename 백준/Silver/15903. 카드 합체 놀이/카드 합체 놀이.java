8c2070d5ac4adcfda37537e8bd46c47cbaa56012
Chan <zxiop12@gmail.com>
2024-03-07 15:23:42 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 124 ms, Memory: 13064 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.add(Long.parseLong(st.nextToken()));
+        }
+
+        for (int i = 0; i < m; i++) {
+            long min1 = pq.poll();
+            long min2 = pq.poll();
+            pq.add(min1 + min2);
+            pq.add(min1 + min2);
+        }
+
+        long result = 0;
+        while (!pq.isEmpty()) {
+            result += pq.poll();
+        }
+        System.out.println(result);
+    }
+}
