32f0bd9158093f25e858688b29294208cbe25d84
Chan <zxiop12@gmail.com>
2024-03-08 14:31:02 +0900

    [level 2] Title: 타겟 넘버, Time: 4.89 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     private int cnt = 0;
 
     public int solution(int[] numbers, int target) {
-        DFS(numbers, 0, 0, target);
+//        DFS(numbers, 0, 0, target);
+        BFS(numbers, target);
         return cnt;
     }
 
@@ -16,5 +20,25 @@ class Solution {
 
         DFS(numbers, cur + 1, result + numbers[cur], target);
         DFS(numbers, cur + 1, result - numbers[cur], target);
+    }
+
+    private void BFS(int[] numbers, int target) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+
+        while (!queue.isEmpty()) {
+            int[] nextInfo = queue.poll();
+            int cur = nextInfo[0];
+            int sum = nextInfo[1];
+
+            if (cur == numbers.length) {
+                if (sum == target) {
+                    cnt++;
+                }
+                continue;
+            }
+            queue.add(new int[]{cur + 1, sum + numbers[cur]});
+            queue.add(new int[]{cur + 1, sum - numbers[cur]});
+        }
     }
 }
